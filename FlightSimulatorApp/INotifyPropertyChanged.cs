4da79fecfdd65fe2c949e7e323999c1e23506bfe
dor <dor@DOR>
2020-03-26 13:30:00 +0200

    temove comments

--- FlightSimulatorApp/INotifyPropertyChanged.cs
+++ FlightSimulatorApp/INotifyPropertyChanged.cs
@@ -5,12 +5,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FlightSimulatorApp
-{        //ניצור דלגייט שיגדיר איך פונקציה שדואגת לשלוח הודעה על שינוי תכונה צריכה להראות.
+namespace FlightSimulatorApp { 
     public delegate void PropertyChangedEventHandler(object sender, PropertyChangedEventArgs e);
     interface INotifyPropertyChanged
     {
-        //ניצור איבנט שכל מי שיממש את הממשק יכיל אותו, והוא יודיע לכל האוברזרברים שרשומים אליו שמשהו השתנה.
         event PropertyChangedEventHandler propertyChanged;
     }
 }
