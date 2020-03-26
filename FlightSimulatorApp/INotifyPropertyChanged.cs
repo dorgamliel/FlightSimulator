dff89094d558425f9e1f8f353c996678f2aea322
dor <dor@DOR>
2020-03-26 12:51:57 +0200

    Testing commit on VS.

---
+++ FlightSimulatorApp/INotifyPropertyChanged.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightSimulatorApp
+{        //ניצור דלגייט שיגדיר איך פונקציה שדואגת לשלוח הודעה על שינוי תכונה צריכה להראות.
+    public delegate void PropertyChangedEventHandler(object sender, PropertyChangedEventArgs e);
+    interface INotifyPropertyChanged
+    {
+        //ניצור איבנט שכל מי שיממש את הממשק יכיל אותו, והוא יודיע לכל האוברזרברים שרשומים אליו שמשהו השתנה.
+        event PropertyChangedEventHandler propertyChanged;
+    }
+}
