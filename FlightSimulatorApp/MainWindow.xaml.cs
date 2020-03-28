90dee43dd29b970219ea76dcd746df4a1f5eaa9a
Alon Shoval <alon_shoval@hotmail.com>
2020-03-28 14:26:34 +0300

    worked on view

--- FlightSimulatorApp/MainWindow.xaml.cs
+++ FlightSimulatorApp/MainWindow.xaml.cs
@@ -18,9 +18,9 @@ namespace FlightSimulatorApp
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow1 : Window
     {
-        public MainWindow()
+        public MainWindow1()
         {
             InitializeComponent();
         }
