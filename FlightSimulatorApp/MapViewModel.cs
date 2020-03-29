ca2fd7a7f4fe2c63663a1200eb6e01bc508db6a2
Alon Shoval <alon_shoval@hotmail.com>
2020-03-29 14:09:49 +0300

    woked on model and map VM

--- FlightSimulatorApp/MapViewModel.cs
+++ FlightSimulatorApp/MapViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,30 @@ namespace FlightSimulatorApp
     class MapViewModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler propertyChanged;
-        private 
+        private IFlightSimulator simulator;
+
+        public MapViewModel(IFlightSimulator sim)
+        {
+            this.simulator = sim;
+            simulator.propertyChanged += delegate (Object sender, PropertyChangedEventArgs e)
+            {
+                NotifyPropertyChanged("VM_" + e.PropertyName);
+            };
+        }
+
+        public void NotifyPropertyChanged(string propName)
+        {
+            if (this.propertyChanged != null)
+                this.propertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+
+        public double Latitude
+        {
+            get { return simulator.Latitude; }
+        }
+        public double Longitude
+        {
+            get { return simulator.Longitude; }
+        }
     }
 }
